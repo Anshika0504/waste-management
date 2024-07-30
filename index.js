1092b25b02d9d20d3dc4535c7192c3a2bf80a692
Anshika Agarwal <anshikaagarwal0405@gmail.com>
2024-07-30 10:13:55 +0530

    getone and delete api has been made

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("./config/db");
 const cors=require('cors');
 const dotenv=require('dotenv');
 const Waste=require('./models/waste');
+const waste = require('./models/waste');
 const app=express();
 app.use(express.json());
 app.use(cors());
@@ -33,5 +34,34 @@ app.get("/getAll",async(req,resp)=>{
         resp.status(500).json({error:"some error is there"});
     }
 
+})
+
+app.get("/getone/:id", async (req, resp) => {
+    try {
+        const id = req.params.id;
+        const wasteExist = await Waste.findById(id);
+        if (!wasteExist) {
+            return resp.status(404).json({ msg: "user not found" });
+        }
+        resp.status(200).json(wasteExist);
+    } catch (error) {
+        resp.status(500).json({ error: "some error has been occurred" });
+    }
+});
+
+
+app.delete("/delete/:id",async(req,resp)=>{
+    try{
+        const id=req.params.id;
+        const wasteExist=await Waste.findById(id);
+        if(!wasteExist){
+            return resp.status(400).json({msg:"waste does not found"});
+        }
+        await Waste.findByIdAndDelete(id);
+        resp.status(200).json({msg:"waste has been deleted successfully"});
+    }
+    catch(error){
+        resp.status(404).json({error:"some unexpected error has been occured"})
+    }
 })
 app.listen(8000);
